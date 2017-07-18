bfbeeb5e06fe4627a62ef9f4b3f72f10fb75e686
Roque Marchio <rmarchio@litoral-gas.com.ar>
2017-07-18 13:04:52 -0300

    .

--- launch.sh
+++ launch.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-    ../Ora2Xml -user=lgas/taxi@sict2 -wenc=iso-8859-1 test.xml
+    ../Ora2Xml -user=lgas/taxi@sict2 -wenc=iso-8859-1 -ofile=salida.xml test.xml
+    cat salida.xml | ./XMLD
+    echo ""
