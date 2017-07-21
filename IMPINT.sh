f2da6014049f027806871e7e5c4b760164444616
martinmarchio <martinmarchio@hotmail.com>
2017-07-21 14:04:15 -0300

    .

--- IMPINT.sh
+++ IMPINT.sh
@@ -1,3 +1,17 @@
+#
+# IMPINT:
+#
+# Para convocar batch
+#
+# Para Oracle
+#
+export NLS_LANG=AMERICAN_AMERICA.WE8ISO8859P1
+#
+Login=lgas/taxi@sict2
+Archivo=impint_doc
+Ejecutable=impint.xml
+
+cat > ${Archivo}.param.xml <<EOF
 <?xml version="1.0" encoding="iso-8859-1" standalone="no" ?>
 <IMPINT_PARAM>
     <NOMBREARCHIVO PARAMFILE="">
@@ -13,3 +27,8 @@
     <ORD_DESDE PARAMORDDSD="10769681">
     <ORD_HASTA PARAMORDHST="10769681">
 </IMPINT_PARAM>
+EOF
+
+../Ora2Xml -user=${login} -wenc=iso-8859-1 -ofile=${Archivo}.out.xml ${Ejecutable} ${Archivo}.param.xml
+cat ${Archivo}.out.xml | ./XMLD
+ echo ""
--- launch.sh
+++ launch.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-    ../Ora2Xml -user=lgas/taxi@sict2 -wenc=iso-8859-1 -ofile=salida.xml test.xml
+    > salida.xml
+    ../Ora2Xml -user=lgas/taxi@sict2 -wenc=iso-8859-1 -ofile=salida.xml impint_doc.xml impint_doc.param.xml
     cat salida.xml | ./XMLD
     echo ""
